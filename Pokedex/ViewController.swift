d15dcae54ebfce0fc47af3976fe20c3491935a4e
smbss <sandro.simes92@gmail.com>
2016-09-29 17:18:58 +0100

    Added a music and pokemon font

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var collection: UICollectionView!
     
     var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +23,20 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collection.delegate = self
         
         parsePokemonCSV()
+    }
+    
+    func initAudio() {
+        
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1 // Infinite loop
+            
+        } catch let err as NSError {
+            print("!ERRmus: \(err.debugDescription)")
+        }
     }
 
     func parsePokemonCSV() {
@@ -75,9 +91,24 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        return CGSize(width: 105, height: 105)
+        return CGSize(width: 90, height: 80)
     }
     
+    @IBAction func musicBtnPressed(_ sender: UIButton) {
+        
+        if musicPlayer == nil {
+            initAudio()
+        }
+        
+        if musicPlayer.isPlaying {
+            musicPlayer.pause()
+            sender.alpha = 0.2
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+    
+    }
     
     
 
