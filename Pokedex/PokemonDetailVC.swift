8b04f0ec0d111b21132b14bcbd57cf76385b4786
smbss <sandro.simes92@gmail.com>
2016-09-30 20:47:57 +0100

    DetailVC layout defined with stack views

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -12,10 +12,9 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     
-    @IBOutlet weak var nameLbl: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameLbl.text = pokemon.name
+        
     }
+    
 }
