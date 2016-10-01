8c8ac567e83101f1f7b975d853009422481fc210
smbss <sandro.simes92@gmail.com>
2016-10-01 20:28:13 +0100

    Added outlets in detailVC and new properties to the pokemon class

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -2,7 +2,7 @@
 //  PokemonDetailVC.swift
 //  Pokedex
 //
-//  Created by Sandro Simes on 29/09/2016.
+//  Created by smbss on 29/09/2016.
 //  Copyright © 2016 smbss. All rights reserved.
 //
 
@@ -12,9 +12,31 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var hightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        nameLbl.text = pokemon.name.capitalized
+        mainImg.image = UIImage(named: "\(pokemon.pokedexId)")
     }
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     
 }
